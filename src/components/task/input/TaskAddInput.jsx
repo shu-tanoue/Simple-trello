1f331ebd94c934e8ad19e9cff0c3427ef242615c
Shu <stbook.syu@icloud.com>
2023-02-08 06:51:42 +0900

    first commit

---
+++ src/components/task/input/TaskAddInput.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { v4 as uuid } from "uuid";
+export const TaskAddInput = ({
+  inputText,
+  setInputText,
+  setTaskList,
+  taskList,
+}) => {
+  const handleSubmit = (e) => {
+    const taskId = uuid();
+    e.preventDefault();
+    if (inputText === "") {
+      return;
+    }
+    setTaskList([
+      ...taskList,
+      {
+        id: taskList.length,
+        draggableId: `task-${taskId}`,
+        text: inputText,
+      },
+    ]);
+    setInputText("");
+  };
+
+  const handleChange = (e) => {
+    setInputText(e.target.value);
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="add a task"
+          className="taskAddInput"
+          onChange={handleChange}
+          value={inputText}
+        />
+      </form>
+    </div>
+  );
+};
